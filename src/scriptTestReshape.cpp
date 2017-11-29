a2c4ca3a690fd167b7392e2cebec681b79ad12ac
Matthew Hassell <hassell.matthew@gmail.com>
2017-11-28 20:00:04 -0500

    working on some template utils

--- src/scriptTestReshape.cpp
+++ src/scriptTestReshape.cpp
@@ -1,8 +1,11 @@
 // test the reshape utility
 
-#include "utils.h"
+#include "utils.hpp"
 
 int main(){
+
+	int rows = 5;
+	int cols = 7;
 
 	int **b;
 	b = new int *[rows];
@@ -10,20 +13,33 @@ int main(){
 		b[i] = new int[cols];
 	}
 
+	int tmp = 0;
 	for(int i = 0; i < rows; i++){
 		for(int j = 0; j < cols; j++){
-			b[i][j] = 2+i+j;
+			b[i][j] = tmp;
+			tmp++;
 		}
 	}
 
 	printMatrix(b,rows,cols);	
+
+	int** c;
+
+	c = reshapeMatrix(b,rows,cols,cols,rows);
+
+	printMatrix(c, cols, rows);
 
 	for(int i = 0; i < rows; i++){
 		delete[] b[i];
 	}
 	delete[] b;
 
-	
+	for(int i = 0; i < cols; i++){
+		delete[] c[i];
+	}
+	delete[] c;
+
+	// printMatrix(c, cols, rows);
 
 
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -36,7 +36,7 @@ void zeroMatrix(T &a, int m, int n){
 
 	for(int i = 0; i < m; i++){
 		for(int j = 0; j < n; j++){
-				a[i][j] = 0;
+			a[i][j] = 0;
 		}
 	}
 	
@@ -44,21 +44,36 @@ void zeroMatrix(T &a, int m, int n){
 
 // reshape a matrix, return a pointer to it (do not delete the old one)
 template <typename T>
-T* reshapeMatrix(T &a, int mold, int nold, int mnew, int nnew){
+T** reshapeMatrix(T &a, int mold, int nold, int mnew, int nnew){
 
 	assert(mold*nold == mnew*nnew);
 
+	T tmp[mold*nold];
+	T** newMatrix = new T*[mnew];
+	for(int i = 0; i < mnew; i++){
+		newMatrix[i] = new T[nnew];
+	}
+
+	// it would be nice to do away with this...
 	for(int i = 0; i < mold; i++){
 		for(int j = 0; j < nold; j++){
-			
+			tmp[i*nold+j] = a[i][j];
 		}
 	}
+
+	for(int i = 0; i < mnew; i++){
+		for(int j = 0; j < nnew; j++){
+			newMatrix[i][j] = tmp[i*nnew+j];
+		}
+	}
+
+	return newMatrix;
 
 }
 
 // transpose a matrix, return a pointer to it (do not delete the old one)
 template <typename T>
-T* transposeMatrix(T &a, int m, int n){
+T** transposeMatrix(T &a, int m, int n){
 	
 	
 
