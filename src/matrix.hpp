d51eccaafb1f5f5d0fae50778e683546d2de601f
Matthew Hassell <hassell.matthew@gmail.com>
2017-12-21 19:33:07 -0500

    some progress on matrix class

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -13,7 +13,8 @@ class Matrix{
 		int nrows();
 		int ncols();
 
-		void transpose();
+		Matrix<T> transpose();
+		void transposeInPlace();
 		void reshape(int, int);
 		void print();
 		void zero();
@@ -28,8 +29,6 @@ class Matrix{
 		int rows;
 		int cols;
 		T** elts;
-		
-
 };
 
 template <typename T>
@@ -92,14 +91,109 @@ void Matrix<T>::print(){
 		}
 		std::cout << std::endl;
 	}
+	std::cout << std::endl;
 
 }
 
 template <typename T>
 T& Matrix<T>::operator()(int i, int j){
-	// need to return a reference for this to work for assignment
+
+	// need to return a reference (T&) for this to work for assignment
 	assert(rows*cols != 0);
 	return elts[i][j];
+
+}
+
+template <typename T>
+void Matrix<T>::reshape(int new_rows, int new_cols){
+
+
+	assert(rows*cols != 0);
+	assert(rows*cols == new_rows*new_cols);
+	
+	T** newMatrix = new T*[new_rows];
+	for(int i = 0; i < new_rows; i++){
+		newMatrix[i] = new T[new_cols];
+	}
+
+	T tmp[rows*cols];
+
+	// it would be nice to do away with this...
+	for(int i = 0; i < cols; i++){
+		for(int j = 0; j < rows; j++){
+			tmp[i*rows+j] = elts[j][i];
+		}
+	}
+
+	for(int i = 0; i < new_cols; i++){
+		for(int j = 0; j < new_rows; j++){
+			newMatrix[j][i] = tmp[i*new_rows+j];
+		}
+	}
+
+	// reassign the object's array to the newMatrix and delete the old 
+	for(int i = 0; i < rows; i++){
+		delete[] elts[i];
+	}
+	delete[] elts;
+
+	elts = newMatrix;
+	rows = new_rows;
+	cols = new_cols;
+
+}
+
+template <typename T> 
+Matrix<T> Matrix<T>::transpose(){
+
+	// does not transpose in place
+
+	assert(rows*cols != 0);
+
+	Matrix<T> transposed(cols,rows);
+	for(int i = 0; i < cols; i++){
+		for(int j = 0; j < rows; j++){
+			transposed(i,j) = elts[j][i];
+		}
+	}
+
+	return transposed;
+
+}
+
+template <typename T>
+void Matrix<T>::transposeInPlace(){
+
+	// transposes in place
+	assert(rows*cols != 0);
+
+	T temp[rows*cols]; // need temp variable b/c of aliasing
+
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < cols; j++){
+			temp[i*cols+j] = elts[i][j];
+		}
+	}
+
+	for(int i = 0; i < rows; i++){
+		delete elts[i];
+	}
+	delete[] elts;
+
+	elts = new T*[cols];
+	for(int i = 0; i < cols; i++){
+		elts[i] = new T[rows];
+	}
+
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < cols; j++){
+			elts[j][i] = temp[i*cols+j];
+		}
+	}
+
+	int swap = rows;
+	rows = cols;
+	cols = swap;
 
 }
 
--- src/scriptTestMatrix.cpp
+++ src/scriptTestMatrix.cpp
@@ -13,6 +13,20 @@ int main(){
 		}
 	}
 
+	std::cout << "The original matrix: " << std::endl;
 	M.print();
+
+	std::cout << "Reshaping the matrix: " << std::endl;
+	M.reshape(4,5);  // checked for leaks with valgrind
+	M.print();   // agrees with Matlab
+
+	std::cout << "Transpose the matrix in place: " << std::endl;
+	M.transposeInPlace();
+	M.print();
+
+	// what's broken here?
+	std::cout << "Transposing and creating a new array " << std::endl;
+	Matrix<int> M2 = M.transpose();
+	M2.print();
 
 }
